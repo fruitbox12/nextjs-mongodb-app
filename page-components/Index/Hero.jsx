004160bf1b3bd6773c696e1f713b48663680a38c
fruitbox12 <dylanwong007@gmail.com>
2023-10-04 19:37:30 -0700

    Update Hero.jsx

--- page-components/Index/Hero.jsx
+++ page-components/Index/Hero.jsx
@@ -8,30 +8,21 @@ const Hero = () => {
     <Wrapper>
       <div>
         <h1 className={styles.title}>
-          <span className={styles.nextjs}>Next.js</span>
-          <span className={styles.mongodb}>MongoDB</span>
-          <span>App</span>
+          <span className={styles.nextjs}>CogitAuth</span>
+          <span className={styles.mongodb}></span>
+          <span>Verify your Drops!</span>
         </h1>
         <Container justifyContent="center" className={styles.buttons}>
           <Container>
             <Link passHref href="/feed">
-              <ButtonLink className={styles.button}>Explore Feed</ButtonLink>
+              <ButtonLink className={styles.button}>Explore Cogita Feeds</ButtonLink>
             </Link>
           </Container>
-          <Spacer axis="horizontal" size={1} />
-          <Container>
-            <ButtonLink
-              href="https://github.com/hoangvvo/nextjs-mongodb-app"
-              type="secondary"
-              className={styles.button}
-            >
-              GitHub
-            </ButtonLink>
+
           </Container>
         </Container>
         <p className={styles.subtitle}>
-          A Next.js and MongoDB web application, designed with simplicity for
-          learning and real-world applicability in mind.
+          
         </p>
       </div>
     </Wrapper>
