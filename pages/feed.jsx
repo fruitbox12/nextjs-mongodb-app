24f242cdbbe1b19b6692e7188ce3f5b3ab359915
fruitbox12 <dylanwong007@gmail.com>
2023-10-09 23:54:41 -0700

    Update feed.jsx

--- pages/feed.jsx
+++ pages/feed.jsx
@@ -1,4 +1,3 @@
-import { Feed } from '@/page-components/Feed';
 import Head from 'next/head';
 
 const FeedPage = () => {
@@ -7,7 +6,13 @@ const FeedPage = () => {
       <Head>
         <title>Feed</title>
       </Head>
-      <Feed />
+      <div style={{ position: 'fixed', top: 0, left: 0, width: '100%', height: '100%' }}>
+        <iframe
+          src="http://3.215.79.139:3000/" // Replace with the URL you want to embed
+          frameBorder="0"
+          style={{ width: '100%', height: '100%' }}
+        ></iframe>
+      </div>
     </>
   );
 };
