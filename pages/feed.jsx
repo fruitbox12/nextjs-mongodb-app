ac51b79308d2d89055a1351f8c9b9f880dae72f5
fruitbox12 <dylanwong007@gmail.com>
2023-10-10 03:24:08 -0700

    Update feed.jsx

--- pages/feed.jsx
+++ pages/feed.jsx
@@ -1,6 +1,20 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 import Head from 'next/head';
 
 const FeedPage = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    // Replace this with your actual authentication logic.
+    const userIsLoggedIn = true; // Change this based on your authentication state
+
+    if (!userIsLoggedIn) {
+      // If the user is not logged in, redirect to the login page
+      router.push('/login'); // Replace '/login' with your actual login page route
+    }
+  }, []);
+
   return (
     <>
       <Head>
