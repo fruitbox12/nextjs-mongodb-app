36ffd4c6ecebcfe18d80123e25390267fb74f769
fruitbox12 <dylanwong007@gmail.com>
2023-10-10 03:16:18 -0700

    Update feed.jsx

--- pages/feed.jsx
+++ pages/feed.jsx
@@ -5,17 +5,12 @@ const FeedPage = () => {
     <>
       <Head>
         <title>Feed</title>
-        <meta
-          http-equiv="Content-Security-Policy"
-          content="default-src 'self' https://7e28-3-215-79-139.ngrok-free.app; style-src 'self' 'unsafe-inline'; script-src 'nonce-123213'; script-src 'unsafe-inline';"
-        />
       </Head>
       <div style={{ position: 'fixed', top: 0, left: 0, width: '100%', height: '100%' }}>
         <iframe
-          src="https://7e28-3-215-79-139.ngrok-free.app/"
-          frameborder="0"
-          style={{ width: '100%', height: '100%', border: 'none', position: 'absolute', top: 0, left: 0 }}
-          allow="ngrok-skip-browser-warning 69420"
+          src="https://218a-3-215-79-139.ngrok-free.app" // Replace with the URL you want to embed
+          frameBorder="0"
+          style={{ width: '100%', height: '100%' }}
         ></iframe>
       </div>
     </>
