679b4778a9bac6c3a9286984ef591815518d5636
fruitbox12 <dylanwong007@gmail.com>
2023-10-10 00:34:41 -0700

    Update feed.jsx

--- pages/feed.jsx
+++ pages/feed.jsx
@@ -8,7 +8,7 @@ const FeedPage = () => {
       </Head>
       <div style={{ position: 'fixed', top: 0, left: 0, width: '100%', height: '100%' }}>
         <iframe
-          src="http://3.215.79.139:3000/" // Replace with the URL you want to embed
+          src="https://test.simplyfi.cloud:3000/" // Replace with the URL you want to embed
           frameBorder="0"
           style={{ width: '100%', height: '100%' }}
         ></iframe>
