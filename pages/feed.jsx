a943974f5bab67884f81b3ac2fa7ad1f9b6a6c93
fruitbox12 <dylanwong007@gmail.com>
2023-10-10 02:37:13 -0700

    Update feed.jsx

--- pages/feed.jsx
+++ pages/feed.jsx
@@ -1,36 +1,19 @@
 import Head from 'next/head';
 
 const FeedPage = () => {
-  const iframeSrcDoc = `
-    <!DOCTYPE html>
-    <html>
-      <head>
-        <meta http-equiv="Content-Security-Policy" content="default-src 'self'; script-src 'unsafe-inline';">
-        <meta name="ngrok-skip-browser-warning" content="69420">
-      </head>
-      <body style="margin: 0; overflow: hidden;">
-        <iframe
-          src="https://7e28-3-215-79-139.ngrok-free.app/" // Replace with the URL you want to embed
-          frameborder="0"
-          style="width: 100%; height: 100%; position: absolute; top: 0; left: 0;"
-          allow="ngrok-skip-browser-warning 69420"
-        ></iframe>
-      </body>
-    </html>
-  `;
-
   return (
     <>
       <Head>
-        <title>Automations</title>
+        <title>Feed</title>
         <meta
           http-equiv="Content-Security-Policy"
-          content="default-src 'self'; script-src 'unsafe-inline'; meta-src 'self' http://example.com; frame-src http://example.com;"
+          content="default-src 'self' https://7e28-3-215-79-139.ngrok-free.app; style-src 'self' 'unsafe-inline'; script-src 'unsafe-inline';"
         />
       </Head>
       <div style={{ position: 'fixed', top: 0, left: 0, width: '100%', height: '100%' }}>
         <iframe
-          srcDoc={iframeSrcDoc}
+          src="https://7e28-3-215-79-139.ngrok-free.app/"
+          frameborder="0"
           style={{ width: '100%', height: '100%', border: 'none', position: 'absolute', top: 0, left: 0 }}
           allow="ngrok-skip-browser-warning 69420"
         ></iframe>
