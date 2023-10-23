794d24eca8f0bdd8cad592393b56f3864ecd6b9e
fruitbox12 <dylanwong007@gmail.com>
2023-10-23 14:33:03 -0700

    Update feed.jsx

--- pages/feed.jsx
+++ pages/feed.jsx
@@ -22,7 +22,7 @@ const FeedPage = () => {
       </Head>
       <div style={{ position: 'fixed', top: 0, left: 0, width: '100%', height: '100%' }}>
         <iframe
-          src="https://024f-3-215-79-139.ngrok-free.app" // Replace with the URL you want to embed
+          src="" // Replace with the URL you want to embed
           frameBorder="0"
           style={{ width: '100%', height: '100%' }}
         ></iframe>
